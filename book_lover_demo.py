ba646ef694a317a63d6342e47dba160722121dda
kurt-jr <nps3cs@virginia.edu>
2024-07-02 17:52:56 -0400

    kansas

---
+++ book_lover_demo.py
@@ -0,0 +1,5 @@
+import mypackages
+import mypackages.booklover.booklover
+lover_books = BookLover('Billy Batson', 'shazam69420@aol.edu', 'comics')
+lover_books.add_book('Flashpoint', 5)
+print(lover_books.num_books_read())
--- mypackages/__init__.py
+++
@@ -1,4 +0,0 @@
-print("imported not deported")
-
-
- 
