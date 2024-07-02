191dc49951d2a98c135c6febac13a9eccda30f17
kurt-jr <167905530+kurt-jr@users.noreply.github.com>
2024-07-02 16:41:32 -0400

    yea

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='packageDemo',
+    version='1.0.0',
+    url='https://github.com/kurt-jr/package-demo',
+    author='Kurt Samuels Jr',
+    author_email='nps3cs@virginia,edu',
+    description='package with 100% accurate prediction',
+    packages=['mypackage'],
+    license = 'MIT'
+    
+)
